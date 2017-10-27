181d9d36de2d662c43551bd02fa01960af69613f
Jonathan Experton <jonathan.experton@gmail.com>
2017-10-26 23:41:00 -0400

    remove static in stats, add timer

--- percolation/PercolationStats.java
+++ percolation/PercolationStats.java
@@ -12,45 +12,22 @@
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.Stopwatch;
 
 public class PercolationStats {
-    private static int n;
-    private static int trials;
-    private static double[] mean;
+    private int trials;
+    private double[] thresholds;
+    private Stopwatch watch;
 
     // perform trials independent experiments on an n-by-n grid
-    public PercolationStats(int n, int trials) {
-        if (n <= 0 || trials <= 0)
+    public PercolationStats(int n, int t) {
+        watch = new Stopwatch();
+
+        if (n <= 0 || t <= 0)
             throw new IllegalArgumentException();
 
-        mean = new double[trials];
-    }
-
-    // sample mean of percolation threshold
-    public static double mean() {
-        return StdStats.mean(mean);
-    }
-
-    // sample standard deviation of percolation threshold
-    public static double stddev() {
-        return StdStats.stddev(mean);
-    }
-
-    // low  endpoint of 95% confidence interval
-    public static double confidenceLo() {
-        return mean() - 1.96 * (stddev() / Math.sqrt(trials));
-    }
-
-    // high endpoint of 95% confidence interval
-    public static double confidenceHi() {
-        return mean() + 1.96 * (stddev() / Math.sqrt(trials));
-    }
-
-    public static void main(String[] args) {
-        n = Integer.parseInt(args[0]);
-        trials = Integer.parseInt(args[1]);
-
-        mean = new double[trials];
+        trials = t;
+        thresholds = new double[trials];
 
         for (int i = 0; i < trials; i++) {
             Percolation perc = new Percolation(n);
@@ -58,14 +35,45 @@ public class PercolationStats {
                 int row = StdRandom.uniform(n) + 1;
                 int col = StdRandom.uniform(n) + 1;
 
-                if (!perc.isOpen(row, col)) {
+                if (!perc.isOpen(row, col))
                     perc.open(row, col);
-                }
             }
-            mean[i] = (double) perc.numberOfOpenSites() / (n * n);
+            thresholds[i] = (double) perc.numberOfOpenSites() / (n * n);
         }
-        StdOut.println("mean\t\t\t= " + mean());
-        StdOut.println("stddev\t\t\t= " + stddev());
-        StdOut.println("95% confidence interval\t= [" + confidenceLo() + ", " + confidenceHi() + "]");
+    }
+
+    // sample mean of percolation threshold
+    public double mean() {
+        return StdStats.mean(thresholds);
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev() {
+        return StdStats.stddev(thresholds);
+    }
+
+    // low  endpoint of 95% confidence interval
+    public double confidenceLo() {
+        return mean() - 1.96 * stddev() / Math.sqrt(trials);
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHi() {
+        return mean() + 1.96 * stddev() / Math.sqrt(trials);
+    }
+
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        int trials = Integer.parseInt(args[1]);
+
+        PercolationStats percStats = new PercolationStats(n, trials);
+
+        StdOut.println("mean\t\t\t= " + percStats.mean());
+        StdOut.println("stddev\t\t\t= " + percStats.stddev());
+        StdOut.println("95% confidence interval\t= [" 
+                           + percStats.confidenceLo() + ", " 
+                           + percStats.confidenceHi() + "]");
+        StdOut.println("avg. time (s)\t\t= " 
+                           + (double)(percStats.watch.elapsedTime() / trials));
     }
 }
